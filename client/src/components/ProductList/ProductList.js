692b5a5bcfd8ded57a48ed1879b0a47c196df8cc
Antonella Denisse Cuenca <fuwa08@hotmail.com>
2025-09-29 08:29:57 -0300

    fix: update import paths for ProductCard and ProductDetail components

--- client/src/components/ProductList/ProductList.js
+++ client/src/components/ProductList/ProductList.js
@@ -1,4 +1,4 @@
-import ProductCard from "../ProductCard";
+import ProductCard from "../ProductCard/ProductCard";
 import "./ProductList.css";
 
 export default function ProductList({ products, onProductClick }) {
