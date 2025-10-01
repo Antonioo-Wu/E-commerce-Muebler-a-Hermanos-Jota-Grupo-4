87068b8b82b24b3f9a6e2d33030404791cb2aba9
MaxawaC <crespinmaxi77@gmail.com>
2025-10-01 18:59:33 -0300

    feat: add notFound middleware to handle 404 routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require('./middlewares/logger');
+const notFound = require('./middlewares/notFound')
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -8,11 +9,16 @@ app.use(logger);
 app.use(cors());
 app.use(express.json());
 
+//Ruta de prueba
 app.get(`/`, (req, res) => {
     res.send('Prueba');
 });
 
-app.post(`/test-post`, (req, res) => res.json({ok:true, body: req.body}));
+app.post(`/test-post`, (req, res) => {
+    res.json({ok:true, body: req.body});
+});
+
+app.use(notFound);
 
 app.listen(PORT, () => {
     console.log(`Sevidor corriendo en ${PORT}`);
