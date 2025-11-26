e97d2254927ea7b085b16bfceee0482c2f939c47
MaxawaC <crespinmaxi77@gmail.com>
2025-11-26 11:18:57 -0300

    fix: agregar manejo de token y llamada a next

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,9 +7,14 @@ const authMiddleware = (req, res, next) => {
         return res.status(401).json({msg: "token no encontrado"});
     }
 
+    const token = authHeader.startsWith("Bearer ")
+    ? authHeader.slice(7)
+    : authHeader;
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = { id: decoded.id, role: decoded.role };
+        next();
     } catch (error) {
         return res.status(401).json({msg: "token invalido o expirado"});
     }
